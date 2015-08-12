d766f4308d5c3bf5514dd012cd7c54240b25fd61
svlcode <vio.slaniceanu@gmail.com>
2015-08-12 15:51:18 +0300

    First commit

---
+++ Sources/OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        OdeToFoodDb _db = new OdeToFoodDb();
+
+        public ActionResult Index(string searchTerm = null)
+        {
+            //var model = from r in _db.Restaurants
+            //            orderby r.Reviews.Average(review => review.Rating) descending 
+            //            select new RestaurantViewModel
+            //            {
+            //                Id = r.Id,
+            //                Name = r.Name,
+            //                City = r.City,
+            //                Country = r.Country,
+            //                CountOfReviews = r.Reviews.Count()
+            //            };
+
+            var model = _db.Restaurants
+                        .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
+                        .Where(r => string.IsNullOrEmpty(searchTerm)|| r.Name.ToLower().StartsWith(searchTerm.ToLower()))
+                        .Take(10)
+                        .Select
+                        (r => new RestaurantViewModel
+                                {
+                                    Id = r.Id,
+                                    Name = r.Name,
+                                    City = r.City,
+                                    Country = r.Country,
+                                    CountOfReviews = r.Reviews.Count()
+                                }
+                        );
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
