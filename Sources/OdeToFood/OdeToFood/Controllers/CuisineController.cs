e7f2fbbc3fce0154062e60b75c0a08278dcd2d71
svlcode <vio.slaniceanu@gmail.com>
2015-08-24 17:17:05 +0300

    Reset code

---
+++ Sources/OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Filters;
+
+namespace OdeToFood.Controllers
+{
+    public class CuisineController : Controller
+    {
+        [Log]
+        public ActionResult Search(string name)
+        {
+            throw new Exception("Something terrible has happend");
+            var message = Server.HtmlEncode(name);
+            return Json(new {Message = name, Name = "Viorel"}, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
