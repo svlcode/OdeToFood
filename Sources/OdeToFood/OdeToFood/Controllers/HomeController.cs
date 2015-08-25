ec3ab1ef120985fe2459b046642b24a2cce880c1
svlcode <vio.slaniceanu@gmail.com>
2015-08-25 17:20:49 +0300

    Added DB support

--- Sources/OdeToFood/OdeToFood/Controllers/HomeController.cs
+++ Sources/OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,16 +10,12 @@ namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        OdeToFoodDb _db = new OdeToFoodDb();
+
         public ActionResult Index()
         {
-            var controller = RouteData.Values["controller"];
-            var action = RouteData.Values["action"];
-            var id = RouteData.Values["id"];
-
-            var message = string.Format("{0}::{1} {2}", controller, action, id);
-            ViewBag.Message = message;
-
-            return View();
+            var model = _db.Restaurants.ToList();
+            return View(model);
         }
 
         public ActionResult About()
@@ -37,6 +34,15 @@ namespace OdeToFood.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
